9a1146376f034cd9a809a0e32e20e04f72422d98
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-14 10:27:53 +0530

    Initial config setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Body from './Body';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Body />, document.getElementById('root'));
+registerServiceWorker();
