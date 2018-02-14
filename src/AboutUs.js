269385cc4f89d9abc2a6405f81bed3cf8ee9b30a
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-14 11:04:07 +0530

    Added Header and About Us components

---
+++ src/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AboutUs.css';
+
+export default class AboutUs extends React.Component {
+  render() {
+    return (
+      <div className="aboutus">
+        <button>About Us</button>
+      </div>
+    );
+  }
+}
